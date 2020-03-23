84a8d199ae7ebd90e82f35cd5364ad2153eb29ec
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-03-23 23:07:32 +0800

    Create initial get_pse_data function

---
+++ R/fastquant.R
@@ -0,0 +1,14 @@
+library(httr)
+
+get_pse_data_by_date <- function(symbol, date){
+  # Gets data for a specific stock for a specific date and returns as a list
+  url <- paste0("http://phisix-api2.appspot.com/stocks/", symbol, ".", date, ".json")
+  url %>% httr::GET() %>% content(type="application/json") -> res
+  dt <- res$as_of
+  stock <- res$stock[[1]]
+  data <- list(dt = res$as_of, name = stock$name, currency = stock$price$currency,
+               close = stock$price$amount, percent_change = stock$percent_change,
+               volume = stock$volume, symbol = stock$symbol)
+  
+  return(data)
+}
