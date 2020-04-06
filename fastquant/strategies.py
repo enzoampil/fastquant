dff7efd0a0fa9358431d44c500808482ba045c1d
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-04-06 18:30:19 +0800

    Add EMA strategy class

--- fastquant/strategies.py
+++ fastquant/strategies.py
@@ -318,6 +318,47 @@ class SMACStrategy(BaseStrategy):
         return self.crossover < 0
 
 
+class EMACStrategy(BaseStrategy):
+    """
+    Exponential moving average crossover strategy
+
+    Parameters
+    ----------
+    fast_period : int
+        The period used for the fast exponential moving average line (should be smaller than `slow_upper`)
+    slow_period : int
+        The period used for the slow exponential moving average line (should be larger than `fast_upper`)
+
+    """
+
+    params = (
+        ("fast_period", 10),  # period for the fast moving average
+        ("slow_period", 30),
+    )
+
+    def __init__(self):
+        # Initialize global variables
+        super().__init__()
+        # Strategy level variables
+        self.fast_period = self.params.fast_period
+        self.slow_period = self.params.slow_period
+
+        print("===Strategy level arguments===")
+        print("fast_period :", self.fast_period)
+        print("slow_period :", self.slow_period)
+        ema_fast = bt.ind.EMA(period=self.fast_period)  # fast moving average
+        ema_slow = bt.ind.EMA(period=self.slow_period)  # slow moving average
+        self.crossover = bt.ind.CrossOver(
+            ema_fast, ema_slow
+        )  # crossover signal
+
+    def buy_signal(self):
+        return self.crossover > 0
+
+    def sell_signal(self):
+        return self.crossover < 0
+
+
 class MACDStrategy(BaseStrategy):
     """
     Moving Average Convergence Divergence (MACD) strategy
@@ -391,6 +432,7 @@ STRATEGY_MAPPING = {
     "smac": SMACStrategy,
     "base": BaseStrategy,
     "macd": MACDStrategy,
+    "emac": EMACStrategy
 }
 
 strat_docs = "\nExisting strategies:\n\n" + "\n".join(
