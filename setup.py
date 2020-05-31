6a62464f2ad99f666e01ea6f348e072603a3ab94
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-06-01 06:58:44 +0800

    Fixing setup issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-with open("requirements.txt", "r") as fh:
+with open("python/requirements.txt", "r") as fh:
     install_requires = fh.read().splitlines()
 
 setuptools.setup(
@@ -13,11 +13,11 @@ setuptools.setup(
     description="Bringing data driven investments to the mainstream",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/enzoampil/fastquant",
+    url="https://github.com/enzoampil/fastquant/python",
     packages=setuptools.find_packages(exclude=["docs", "tests"]),
     package_data={"fastquant": ["data/*"]},
     include_package_data=True,
-    scripts=["scripts/get_disclosures", "scripts/update_cache"],
+    scripts=["python/scripts/get_disclosures", "python/scripts/update_cache"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
