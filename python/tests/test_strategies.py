eae8bdede3e7f2ee6dec5925e3dd016a57655ee9
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-06-16 17:23:37 +0800

    Add multi-strategy test

--- python/tests/test_strategies.py
+++ python/tests/test_strategies.py
@@ -3,6 +3,10 @@ from pathlib import Path
 from fastquant import backtest, STRATEGY_MAPPING, DATA_PATH
 
 SAMPLE_CSV = Path(DATA_PATH, "JFC_20180101_20190110_DCV.csv")
+SAMPLE_STRAT_DICT = {
+    "smac": {"fast_period": 35, "slow_period": [40, 50]},
+    "rsi": {"rsi_lower": [15, 30], "rsi_upper": 70},
+}
 
 
 def test_backtest():
@@ -16,3 +20,9 @@ def test_backtest():
             strategy
         )
         assert cerebro is not None, errmsg
+
+    # Test multi-strategy
+    cerebro = backtest("multi", sample, strats=SAMPLE_STRAT_DICT, plot=False)
+    assert (
+        cerebro is not None
+    ), "Backtest encountered error for strategy 'multi'!"
