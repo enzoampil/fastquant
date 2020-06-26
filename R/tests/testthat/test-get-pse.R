bcd6aece7f40a5db034d5ea6639b4368ac3f8883
Jose Endrinal <francis.endrinal@gmail.com>
2020-06-27 00:20:48 +0800

    Test for data correctness

--- R/tests/testthat/test-get-pse.R
+++ R/tests/testthat/test-get-pse.R
@@ -7,3 +7,8 @@ test_that("get_pse_data returns tibble", {
   expect_s3_class(df, "tbl")
   expect_s3_class(df, "data.frame")
 })
+
+test_that("get_pse_data returns correct data", {
+  expect_equal(get_pse_data("JFC", "2019-01-03", "2019-01-03")$close, 292)
+  expect_equal(get_pse_data("JFC", "2019-01-03", "2019-01-03")$volume, 1665440)
+})
