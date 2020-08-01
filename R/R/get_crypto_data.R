fc14af798e4478f0256886875f256f5ac6c31020
Jose Endrinal <francis.endrinal@gmail.com>
2020-08-01 23:37:15 +0800

    Add crypto endpoint

---
+++ R/R/get_crypto_data.R
@@ -0,0 +1,19 @@
+#' Returns price data for cypto symbol through Binance API
+#'
+#' @param symbol A string indicating the ticker symbol of the cryptocurrency in
+#'   the Binance exchange
+#' @param start_date A string indicating a date in the YYYY-mm-dd format, serves
+#'   as the start date of the period to get crypto data
+#' @param end_date A string indicating a date in the YYYY-mm-dd format, serves
+#'   as the end date of the period to get crypto data
+#'
+#' @return A tibble
+#'
+#' @importFrom httr GET content
+#'
+#' @export
+get_crypto_data <- function(symbol, start_date, end_date) {
+  endpoint <- "https://api.binance.com/api/v3/klines?"
+  start_date <- format(as.numeric(as.POSIXct(start_date)) * 1e3, scientific = FALSE)
+  end_date <- format(as.numeric(as.POSIXct(end_date)) * 1e3, scientific = FALSE)
+}
