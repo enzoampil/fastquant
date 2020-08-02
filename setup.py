63f88b99151ec96599e7df233d029801c3637879
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-08-02 09:09:28 +0800

    Update setup script

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("python/requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="fastquant",
-    version="0.1.3.14",
+    version="0.1.3.16",
     author="Lorenzo Ampil",
     author_email="lorenzo.ampil@gmail.com",
     description="Bringing data driven investments to the mainstream",
