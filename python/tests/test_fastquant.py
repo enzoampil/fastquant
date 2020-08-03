7c1bcdeca0f981778701734af3b98ac41475e52b
Beato Bongco <beatobongco@gmail.com>
2020-08-03 20:40:36 +0800

    Add tests for different crypto exchanges
    Format with black

--- python/tests/test_fastquant.py
+++ python/tests/test_fastquant.py
@@ -37,12 +37,19 @@ def test_get_stock_data():
     assert isinstance(stock_df, pd.DataFrame)
 
     # Test getting yahoo when (default) phisix fails on a non PSE SYMBOL
-    stock_df = get_stock_data(
-        YAHOO_SYMBOL, DATE_START, DATE_END
-    )
+    stock_df = get_stock_data(YAHOO_SYMBOL, DATE_START, DATE_END)
     assert isinstance(stock_df, pd.DataFrame)
 
 
 def test_get_crypto_data():
+    # try default crypto exchange, which is binance
     crypto_df = get_crypto_data(CRYPTO_SYMBOL, DATE_START, DATE_END)
     assert isinstance(crypto_df, pd.DataFrame)
+    # test other exchanges work
+    from fastquant.crypto import CRYPTO_EXCHANGES
+
+    for ex in CRYPTO_EXCHANGES:
+        crypto_df = get_crypto_data(
+            CRYPTO_SYMBOL, DATE_START, DATE_END, exchange=ex
+        )
+        assert isinstance(crypto_df, pd.DataFrame)
