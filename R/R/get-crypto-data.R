85086332117f8b0e21e9b76837f0c08682a6c7ad
Jose Endrinal <francis.endrinal@gmail.com>
2020-08-11 14:34:09 +0800

    Get binance cryptocurrency data for fastquant

--- R/R/get-crypto-data.R
+++ R/R/get-crypto-data.R
@@ -11,21 +11,32 @@
 #'
 #' @importFrom httr GET content
 #' @importFrom stringr str_c
-#' @importFrom magrittr str_c
+#' @importFrom magrittr `%>%`
+#' @importFrom dplyr mutate select
+#' @importFrom purrr map_dbl
 #'
 #' @export
 get_crypto_data <- function(symbol, start_date, end_date) {
   endpoint <- "https://api.binance.com/api/v3/klines?"
-  start_date <- format(as.numeric(as.POSIXct(start_date)) * 1e3, scientific = FALSE)
-  end_date <- format(as.numeric(as.POSIXct(end_date)) * 1e3, scientific = FALSE)
+  s_date <- format(as.numeric(as.POSIXct(start_date)) * 1e3, scientific = FALSE)
+  e_date <- format(as.numeric(as.POSIXct(end_date)) * 1e3, scientific = FALSE)
 
   params <- paste(stringr::str_c("symbol=", symbol),
                   "interval=1d",
-                  stringr::str_c("startTime=", start_date),
-                  stringr::str_c("endTime=", end_date),
+                  stringr::str_c("startTime=", s_date),
+                  stringr::str_c("endTime=", e_date),
                   sep = "&")
   res <- httr::GET(stringr::str_c(endpoint, params)) %>%
     httr::content("parsed")
 
-  res
+  x <- NULL
+
+  tibble(dt = seq(as.Date(start_date), as.Date(end_date) - 1, by = "day"),
+         x = res) %>%
+    mutate(open = map_dbl(x, function(x) as.numeric(x[[2]])),
+           high = map_dbl(x, function(x) as.numeric(x[[3]])),
+           low = map_dbl(x, function(x) as.numeric(x[[4]])),
+           close = map_dbl(x, function(x) as.numeric(x[[5]])),
+           volume = map_dbl(x, function(x) as.numeric(x[[6]]))) %>%
+    select(-x)
 }
