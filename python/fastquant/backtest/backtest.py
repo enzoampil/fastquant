fa94bad2b21d65180915eee7a9d145e2b6b4c46e
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-08-13 20:26:03 +0800

    Add index datetime handling

--- python/fastquant/backtest/backtest.py
+++ python/fastquant/backtest/backtest.py
@@ -175,14 +175,15 @@ def backtest(
         )
         data = data.reset_index()
 
-    # If data has `dt` as the index, set `dt` as the first column
+    # If data has `dt` as the index and `dt` and `datetime` are not already columns, set `dt` as the first column
     # This means `backtest` supports the dataframe whether `dt` is the index or a column
-    if data.index.name == "dt":
-        data = data.reset_index()
-    # If the index is a datetime index, set this as the datetime column
-    elif isinstance(data.index, pd.DatetimeIndex):
-        data.index.name = "dt"
-        data = data.reset_index()
+    if set(["dt", "datetime"]).intersection(data.columns):
+        if data.index.name == "dt":
+            data = data.reset_index()
+        # If the index is a datetime index, set this as the datetime column
+        elif isinstance(data.index, pd.DatetimeIndex):
+            data.index.name = "dt"
+            data = data.reset_index()
 
     # Rename "dt" column to "datetime" to match the formal alias
     data = data.rename(columns={"dt": "datetime"})
