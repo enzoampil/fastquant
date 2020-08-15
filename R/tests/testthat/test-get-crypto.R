ed0b55b7c0b242c6597752cf8999184cc33490e4
Jose Endrinal <francis.endrinal@gmail.com>
2020-08-15 17:28:51 +0800

    Revise test

--- R/tests/testthat/test-get-crypto.R
+++ R/tests/testthat/test-get-crypto.R
@@ -9,10 +9,10 @@ test_that("get_stock_data returns tibble", {
 })
 
 test_that("get_crypto_data returns correct data", {
-  expect_equal(get_crypto_data("BTCUSDT", "2018-12-01", "2018-12-02")$close,
+  expect_equal(get_crypto_data("BTCUSDT", "2018-12-01", "2018-12-01")$close,
                4190.02,
                tolerance = 0.01)
-  expect_equal(get_crypto_data("BTCUSDT", "2018-12-01", "2018-12-02")$volume,
+  expect_equal(get_crypto_data("BTCUSDT", "2018-12-01", "2018-12-01")$volume,
                44840.07,
                tolerance = 0.01)
 })
