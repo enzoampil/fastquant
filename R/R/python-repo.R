c8393833d0f1b619537c23cfd31ad773d98e073c
Jose Endrinal <francis.endrinal@gmail.com>
2020-08-17 22:31:30 +0800

    Download Python to client from source

--- R/R/python-repo.R
+++ R/R/python-repo.R
@@ -1,4 +1,6 @@
 #' Download python repository
+#'
+#' @importFrom utils download.file unzip
 #' @keywords internal
 download_fastquant_repo <- function() {
   download.file("https://github.com/enzoampil/fastquant/archive/master.zip",
