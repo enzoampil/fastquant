6e15fe984f8c5b1f09582f49fe428239052d45b8
Jose Endrinal <francis.endrinal@gmail.com>
2020-08-17 14:33:17 +0800

    Install python fastquant dependencies

---
+++ R/R/python-deps.R
@@ -0,0 +1,6 @@
+#' Check and install python dependencies
+#' @keywords internal
+install_python_deps <- function() {
+  deps <- readr::read_lines("~/.fastquant/fastquant/python/requirements.txt")
+  virtualenv_install("fastquant-env", deps, ignore_installed = TRUE)
+}
