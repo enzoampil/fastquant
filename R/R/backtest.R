b3935e9b17df89256f1e56d189409d212b1e47f9
Jose Endrinal <francis.endrinal@gmail.com>
2020-09-01 21:40:59 +0800

    Coerce ellipsis to list

--- R/R/backtest.R
+++ R/R/backtest.R
@@ -4,10 +4,19 @@
 #' @param strat Character string indicating the strategy to backtest
 #' @param ... Paramters to be passed on to the Python backtest function
 #'
+#' @importFrom rlang list2
+#'
 #' @export
 backtest <- function(data, strat, ...) {
+  kwargs <- list2(...)
 
-  result <- fq$backtest(strat, data, ...)
+  kwargs$strategy <- strat
+  kwargs$data <- data
+
+  kwargs$verbose <- FALSE
+  kwargs$plot <- FALSE
+
+  result <- do.call(what = fq$backtest, args = kwargs)
   return(paste0("Returns ", typeof(result)))
 
 }
