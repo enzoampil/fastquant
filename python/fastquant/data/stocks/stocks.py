19a1ad57f187d027f2c2ae07034e747b7334f06b
rafmacalaba <rafael.macalaba@yahoo.com>
2020-09-20 11:58:40 +0800

    standardize format to ohlcv

--- python/fastquant/data/stocks/stocks.py
+++ python/fastquant/data/stocks/stocks.py
@@ -47,10 +47,10 @@ def get_stock_data(
         # The query is run on 'yahoo', but if the symbol isn't found, the same query is run on 'phisix'.
         df = get_yahoo_data(symbol, start_date, end_date)
         if df is None:
-            df = get_pse_data(symbol, start_date, end_date, format="c")
+            df = get_pse_data(symbol, start_date, end_date, format=format)
     elif source == "phisix":
         # The query is run on 'phisix', but if the symbol isn't found, the same query is run on 'yahoo'.
-        df = get_pse_data(symbol, start_date, end_date, format="c")
+        df = get_pse_data(symbol, start_date, end_date, format=format)
         if df is None:
             df = get_yahoo_data(symbol, start_date, end_date)
     else:
