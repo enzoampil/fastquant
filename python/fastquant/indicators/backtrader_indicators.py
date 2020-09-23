12bd3211cf8fa63363123693aa5dc9a8931b05f9
jbdelmundo <jbdelmundo@gmail.com>
2020-09-23 17:16:33 +0800

    Import indicators from backtrader to fastquant

---
+++ python/fastquant/indicators/backtrader_indicators.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Import standard library
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+# Indicators from backtrader
+from backtrader.indicators import (
+    AverageDirectionalMovementIndex,
+    ADX,
+    BollingerBands,
+    BBands,
+    CommodityChannelIndex,
+    CCI,
+    ExponentialMovingAverage,
+    EMA,
+    Ichimoku,
+    MACD,
+    RelativeStrengthIndex,
+    RSI,
+    SimpleMovingAverage,
+    SMA,
+    Stochastic,
+    TripleExponentialMovingAverage,
+    TRIX,
+    WilliamsR,
+)
