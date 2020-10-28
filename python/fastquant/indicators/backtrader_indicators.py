2cedc15a8eaffe22cfb3ee002df82d3a9fc2b94b
scottmgoley@gmail.com <scottmgoley@gmail.com>
2020-10-28 11:35:37 -0400

    Add ATR to imported backtrader indicators list

--- python/fastquant/indicators/backtrader_indicators.py
+++ python/fastquant/indicators/backtrader_indicators.py
@@ -12,6 +12,7 @@ from __future__ import (
 from backtrader.indicators import (
     AverageDirectionalMovementIndex,
     ADX,
+    AverageTrueRange,
     BollingerBands,
     BBands,
     CommodityChannelIndex,
