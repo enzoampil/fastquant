0f2d5d77b93cf86a4987e48deaa63e95fb837775
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-11-01 09:42:14 +0800

    Correct indexing of self.datas to -1 instead of + 1

--- python/fastquant/strategies/atr.py
+++ python/fastquant/strategies/atr.py
@@ -46,7 +46,7 @@ class ATRStrategy(BaseStrategy):
         self.dataclose = self.datas[0].close
         self.dataopen = self.datas[0].open
         self.lastdataclose = self.datas[-1].close
-        self.lastdataopen = self.datas[1].open
+        self.lastdataopen = self.datas[-1].open
 
         self.atr_trailing_stop = 0
 
