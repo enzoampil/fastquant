bdd9120ad0859ae6707114151f6c2e158f343cbd
Jose Endrinal <francis.endrinal@gmail.com>
2020-11-07 17:20:24 +0800

    Remove R code from folder

--- R/R/backtest.R
+++
@@ -1,1 +0,0 @@
-#' Backtest a trading strategy
--- R/R/python-use.R
+++
@@ -1,9 +0,0 @@
-#' Declare custom python version
-#'
-#' @param python Path to Python binary
-#' @param required 	Is this version of Python required? If TRUE then an error
-#'   occurs if it's not located. Otherwise, the version is taken as a hint only
-#'   and scanning for other versions will still proceed.
-#'
-#' @export
-use_python <- reticulate::use_python
