6ee9a19365812f89ffb6c0f53dd4c5213cb9a325
Alan Jones <alan@schmutza.com>
2021-04-24 11:39:18 -0700

    Pull the full date range requested in pages. Ignore vscode config data directory

--- python/fastquant/data/crypto/crypto.py
+++ python/fastquant/data/crypto/crypto.py
@@ -38,14 +38,58 @@ def get_crypto_data(ticker, start_date, end_date, time_resolution="1d", exchange
        market exchanges: 'binance' (default), 'coinbasepro', 'bithumb', 'kraken', 'kucoin', 'bitstamp'
     """
     start_date_epoch = unix_time_millis(start_date)
+    end_date_epoch = unix_time_millis(end_date)
 
     if exchange in CRYPTO_EXCHANGES:
+        # Get the exchange we want to use from ccxt's exchange attributes
         ex = getattr(ccxt, exchange)({"verbose": False})
-        ohlcv_lol = ex.fetch_ohlcv(ticker, time_resolution, since=start_date_epoch)
-        ohlcv_df = pd.DataFrame(
-            ohlcv_lol, columns=["dt", "open", "high", "low", "close", "volume"]
-        )
+
+        # We're going to get data in batches, so want to know what the last record in the previous batch was
+        previous_request_end_date_epoch = start_date_epoch
+
+        # The time we want data from will shift each batch as well
+        request_start_date_epoch = start_date_epoch
+
+        # Variable to store our dataframe as we fill it out
+        ohlcv_df = None
+
+        while (previous_request_end_date_epoch < end_date_epoch):
+            # Pull the data from the exchange
+            ohlcv_lol = ex.fetch_ohlcv(ticker, time_resolution, since=request_start_date_epoch)
+            # Convert it to a dataframe
+            current_request_df = pd.DataFrame(
+                ohlcv_lol, columns=["dt", "open", "high", "low", "close", "volume"]
+            )
+
+            if ohlcv_df is None:
+                # We don't have a dataframe yet, so start with this
+                ohlcv_df = current_request_df
+            else:
+                # Append the results to what we have so far
+                ohlcv_df = ohlcv_df.append(current_request_df)
+            
+            # Get the last entry timestamp after we've retrieved (or attempted to) additional records
+            current_request_end_date_epoch = int(ohlcv_df.tail(1).dt.values[0])
+
+            if current_request_end_date_epoch <= previous_request_end_date_epoch:
+                # We haven't gained any additional records, so there's no point in further requests
+                # Let's mark this for the data end date, mostly so both end_date and end_date_epoch will be
+                # in sync in case someone in future uses them in code futher down and to ensure the loop bails
+                end_date_epoch = current_request_end_date_epoch
+                # Update the actual end date so that the stored value will reflect the actual end
+                end_date = pd.to_datetime(end_date_epoch, unit="ms")
+                # The loop would exit based on the end_date_epoch value, but we'll save that check occuring
+                break
+            else:
+                # We've gained some more records, so let's place another request (unless we're past the end, but our loop will catch that without checking here)
+                # The next request should start a millisecond after this one ended
+                request_start_date_epoch = current_request_end_date_epoch + 1
+                # This request's end date should now be set as current for the next loop
+                previous_request_end_date_epoch = current_request_end_date_epoch
+        
+        # Convert the unix timestampe to datetime
         ohlcv_df["dt"] = pd.to_datetime(ohlcv_df["dt"], unit="ms")
+        # Trim off any records which were returned beyond the end
         ohlcv_df = ohlcv_df[ohlcv_df.dt <= end_date]
         # Save input parameters into dataframe
         ohlcv_df.start_date = start_date
